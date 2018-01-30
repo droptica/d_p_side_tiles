c8b6eb9a7777fe93de43d3ff9ec2b462254b5d8d
grzegorz-pietrzak-droptica <grzegorz.pietrzak@droptica.pl>
2018-01-30 20:20:01 +0100

    Working version.

--- js/d_p_side_tiles.js
+++ js/d_p_side_tiles.js
@@ -4,11 +4,34 @@
 
   Drupal.behaviors.droopler_subtheme = {
     attach: function (context, settings) {
+
       // Enable Masonry.
-      $('.d-side-tiles').masonry({
-        itemSelector: '.d-side-tiles-item',
-        columnWidth: '.d-side-tiles-sizer',
-        percentPosition: true,
+      $(document).ready(function() {
+        var $grid = $('.d-side-tiles').masonry({
+          itemSelector: '.d-tiles-item',
+          columnWidth: '.d-tiles-sizer',
+          percentPosition: true,
+        });
+
+        $(window).on('load', function(){
+          $grid.masonry('layout');
+        });
+
+        // Add titles to items.
+        $('.d-side-tiles .d-tiles-item').each(function(){
+          var alt = $(this).find('img').attr('alt');
+          var subalt = '';
+          var parts = alt.split('/', 2);
+          if (parts.length > 1) {
+            alt = parts[0];
+            subalt = parts[1];
+          }
+          var $caption_wrapper = $('<div class="d-tiles-caption"></div>').appendTo($(this));
+          var $caption = $('<div></div>').appendTo($caption_wrapper).text(alt);
+          if (subalt !== '') {
+            $('<small></small>').appendTo($caption).text(subalt);
+          }
+        });
       });
     }
   };
